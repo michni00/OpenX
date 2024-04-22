619fd6c50eff546a0d6e4558bc76050ec35ad9ac
michni00 <michni000@gmail.com>
2024-04-22 12:06:44 +0200

    added fastapi base

---
+++ backend/app.py
@@ -0,0 +1,30 @@
+import uuid
+from fastapi import FastAPI, Body
+from pydantic import BaseModel
+from backend.methods import fahrenheit_to_celsius
+app = FastAPI()
+
+def generate_app_id():
+    app.state.app_id = str(uuid.uuid4())
+
+class TemperatureIn(BaseModel):
+    fahrenheit: float
+    
+class TemperatureOut(BaseModel):
+    celsius: float
+    app_id: str
+
+
+@app.on_event("startup")
+async def startup_event():
+    generate_app_id()
+
+@app.get("/")
+def home():
+    return {"everything is": "OK"}
+
+@app.post("/convert", response_model=TemperatureOut)
+async def convert_temperature(temp: TemperatureIn = Body(...)):
+    fahrenheit = temp.fahrenheit
+    return {"celsius": fahrenheit_to_celsius(fahrenheit), 
+            "app_id": app.state.app_id}
